739ddec47566aceb7b7250e075ff2aacdc660d6b
shaojung <shaojung@gmail.com>
2017-02-18 13:51:15 +0800

    use Save and Load

--- app/src/main/java/com/example/student/db021803/MainActivity.java
+++ app/src/main/java/com/example/student/db021803/MainActivity.java
@@ -3,17 +3,35 @@ package com.example.student.db021803;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText ed;
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+    }
+    public void clickSave(View v)
+    {
         SharedPreferences sp = getSharedPreferences("MYDATA", MODE_PRIVATE);
         SharedPreferences.Editor editor = sp.edit();
-        editor.putString("username", "Bob");
+        ed = (EditText) findViewById(R.id.editText);
+        String s = ed.getText().toString();
+        editor.putString("username", s);
         editor.commit();
+    }
+
+    public void clickLoad(View v)
+    {
+        SharedPreferences sp = getSharedPreferences("MYDATA", MODE_PRIVATE);
+        String u = sp.getString("username", "UserName");
+        tv = (TextView) findViewById(R.id.textView2);
+        tv.setText(u);
     }
 }
